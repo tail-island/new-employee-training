a50c662265d52e35225b3108ec8a72946284687a
OJIMA Ryoji <rojima1@gmail.com>
2022-06-12 14:56:09 +0900

    first release.

--- src/main/java/OnePlusOne.java
+++
@@ -1,5 +0,0 @@
-class OnePlusOne {
-    public static void main(String[] args) {
-        System.out.println(1 + 1);
-    }
-}
--- src/main/java/com/tail_island/training/App.java
+++
@@ -1,7 +0,0 @@
-package com.tail_island.training;
-
-class App {
-    static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
